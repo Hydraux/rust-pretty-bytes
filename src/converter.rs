2b1d304a8d221f29cfb2f2fa15cb98ed09792405
ryan <ryan@cado-pc.lan>
2024-05-31 20:36:01 -0500

    feat: add unit specified rounding
    Adds a function allowing implementors the ability to round to the
    hundredths place of a specified unit.

--- src/converter.rs
+++ src/converter.rs
@@ -1,15 +1,58 @@
-use std::cmp;
+use std::{cmp, ops::Index};
 
 pub fn convert(num: f64) -> String {
-  let negative = if num.is_sign_positive() { "" } else { "-" };
-  let num = num.abs();
-  let units = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-  if num < 1_f64 {
-      return format!("{}{} {}", negative, num, "B");
-  }
-  let delimiter = 1000_f64;
-  let exponent = cmp::min((num.ln() / delimiter.ln()).floor() as i32, (units.len() - 1) as i32);
-  let pretty_bytes = format!("{:.2}", num / delimiter.powi(exponent)).parse::<f64>().unwrap() * 1_f64;
-  let unit = units[exponent as usize];
-  format!("{}{} {}", negative, pretty_bytes, unit)
+    let negative = if num.is_sign_positive() { "" } else { "-" };
+    let num = num.abs();
+    let units = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    if num < 1_f64 {
+        return format!("{}{} {}", negative, num, "B");
+    }
+    let delimiter = 1000_f64;
+    let exponent = cmp::min(
+        (num.ln() / delimiter.ln()).floor() as i32,
+        (units.len() - 1) as i32,
+    );
+    let pretty_bytes = format!("{:.2}", num / delimiter.powi(exponent))
+        .parse::<f64>()
+        .unwrap()
+        * 1_f64;
+    let unit = units[exponent as usize];
+    format!("{}{} {}", negative, pretty_bytes, unit)
+}
+
+#[derive(Debug, Clone)]
+pub enum Unit {
+    B,
+    kB,
+    MB,
+    GB,
+    TB,
+    PB,
+    EB,
+    ZB,
+    YB,
+}
+
+///Converts bytes to a readable format rounding to the nearest hundedths place of a specified unit.
+///
+/// # Arguments
+/// * `num` - number to convert
+/// * `smallest_unit` - smallest unit to round to
+///
+/// # Examples
+/// ```convert_and_round(1234.0, kB)``` => 1.23 kB
+///
+/// ```convert_and_round(234.0, kB)``` => 0.23 kB
+///
+/// ```convert_and_round(4.0, kB)``` => 0.00 kB
+///
+/// ```convert_and_round(1000000, kB)``` => 1 MB
+pub fn convert_and_round(num: f64, smallest_unit: Unit) -> String {
+    let power = smallest_unit.clone() as u8 as f64;
+    let converted_num = num / 1000_f64.powf(power);
+    if converted_num < 1.0 {
+        let converted_num = num / 1000_f64.powf(power);
+        return format!("{:.2} {:?}", converted_num, smallest_unit);
+    }
+    convert(num)
 }
--- tests/converter.rs
+++ tests/converter.rs
@@ -1,6 +1,7 @@
 extern crate pretty_bytes;
 
-use pretty_bytes::converter::convert;
+use pretty_bytes::converter::Unit;
+use pretty_bytes::converter::{convert, convert_with_smallest_unit};
 
 #[test]
 fn it_converts_bytes_to_human_readable_strings() {
@@ -22,4 +23,14 @@ fn it_supports_negative_numbers() {
     assert_eq!(convert(-10.1_f64), "-10.1 B");
     assert_eq!(convert(-999_f64), "-999 B");
     assert_eq!(convert(-1001_f64), "-1 kB");
+}
+
+#[test]
+fn it_supports_smallest_unit_specifications() {
+    assert_eq!(convert_with_smallest_unit(1234.0, Unit::kB), "1.23 kB");
+    assert_eq!(convert_with_smallest_unit(4.0, Unit::kB), "0.00 kB");
+    assert_eq!(convert_with_smallest_unit(1000000.0, Unit::kB), "1 MB");
+    assert_eq!(convert_with_smallest_unit(1230000.0, Unit::kB), "1.23 MB");
+    assert_eq!(convert_with_smallest_unit(230.0, Unit::kB), "0.23 kB");
+    assert_eq!(convert_with_smallest_unit(230000.0, Unit::MB), "0.23 MB");
 }
